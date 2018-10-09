f55f1172612e0645b4a23a59440a3d67cf639f07
Atli Gislason <atlig17@ru.is>
2018-10-09 17:02:55 +0000

    Test 3 works. part 1 finished

--- calculator.js
+++ calculator.js
@@ -2,6 +2,14 @@ function add(numbers) {
     if(numbers == ""){
         return 0;
     }
+    else if(numbers.includes(",")){
+        list_of_numbers = numbers.split(",");
+        var sum = 0;
+        list_of_numbers.forEach(number => {
+            sum += parseInt(number);
+        });
+        return sum;
+    }
     else{
         return parseInt(numbers);
     }
--- calculator.test.js
+++ calculator.test.js
@@ -6,8 +6,15 @@ it("should return 0 on empty string", () => {
 });
 
 //Test 2
-it("should return number taht is inputted", () => {
+it("should return number that is inputted", () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
     expect(add("573")).toBe(573);
+});
+
+//Test 3
+it("should return the sum of the numbers between the commas", () => {
+    expect(add("2,2")).toBe(4);
+    expect(add("55,72")).toBe(127);
+    expect(add("444,555")).toBe(999);
 });
